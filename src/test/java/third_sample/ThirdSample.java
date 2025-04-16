f367195c488d72c51ecfd4815ce2a1ddc0c36ae2
Laxmi Priya Behera <Laxmi Priya Behera@192.168.1.2>
2025-04-17 00:16:54 +0530

    PUSH 2

--- src/test/java/third_sample/ThirdSample.java
+++ src/test/java/third_sample/ThirdSample.java
@@ -3,6 +3,8 @@ package third_sample;
 public class ThirdSample {
 	public static void main(String[] args) {
 		System.out.println("I'm from Third_Sample Project");
+		System.out.println("I'm from Third_Sample Project");
+
 
 	}
 	
